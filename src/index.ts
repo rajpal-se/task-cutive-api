6a66ea7cec5ecb1360664e1497c9c6e6aea21020
Rajpal Singh <rajpal426a@gmail.com>
2026-03-06 22:10:13 +0530

    tsx and node server added.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
-const message: string = "Hi There";
+import express, { Request, Response } from 'express';
 
-console.log(message);
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('API is running 🚀');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
