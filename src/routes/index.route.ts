bca683fc82cbd55e6d588ba7a853e8c42d8182da
Rajpal Singh <rajpal426a@gmail.com>
2026-03-07 14:23:37 +0530

    `/health` route added with complete implementation.

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -2,11 +2,14 @@ import { Router } from 'express';
 import usersRouter from './users.route.js';
 import authRouter from './auth.route.js';
 import tasksRouter from './tasks.route.js';
+import { healthCheck } from '../controllers/health.contoller.js';
 
 const indexRouter = Router();
 
 indexRouter.use('/auth', authRouter);
 indexRouter.use('/users', usersRouter);
 indexRouter.use('/tasks', tasksRouter);
+
+indexRouter.get('/health', healthCheck);
 
 export default indexRouter;
