7ddcb4dddeb61d4d3e88d9323ed355ed2532242f
Rajpal Singh <rajpal426a@gmail.com>
2026-03-07 16:20:27 +0530

    MongoDB connection setup done.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { configDotenv } from 'dotenv';
 import express from 'express';
 import indexRouter from './routes/index.route.js';
+import { connectDB } from './db/connect-db.js';
+import { envSchema } from './validators/app.js';
 
 configDotenv();
 
@@ -11,6 +13,25 @@ app.use(express.json());
 
 app.use('/v1', indexRouter);
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+async function runApp() {
+    try {
+        await envSchema.validate(process.env, { abortEarly: true });
+    } catch (error) {
+        console.error('Environment variables validation error:', error);
+        process.exit(1);
+    }
+
+    try {
+        await connectDB(process.env.MONGO_URI!);
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.error('Failed to connect to MongoDB:', error);
+        process.exit(1);
+    }
+
+    app.listen(PORT, () => {
+        console.log(`Server running on http://localhost:${PORT}`);
+    });
+}
+
+runApp();
---
+++ src/validators/app.ts
@@ -0,0 +1,8 @@
+import * as yup from 'yup';
+
+export const envSchema = yup
+    .object({
+        PORT: yup.string().required('PORT is required').matches(/^\d+$/, 'PORT must be a number'),
+        MONGO_URI: yup.string().required('MONGO_URI is required'),
+    })
+    .required();
